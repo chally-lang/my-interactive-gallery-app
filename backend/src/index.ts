cc1b3b2be19d6e3f244ba399a6d45feef8f51f04
chally-lang <54439978+chally-lang@users.noreply.github.com>
2025-07-07 17:55:56 +0100

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,10 @@ dotenv.config();
 
 const PORT = parseInt(process.env.PORT as string, 10) || 5000;
 
+app.get("/", (req, res) => {
+  res.send("🎉 Interactive Gallery API is live!");
+});
+
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
