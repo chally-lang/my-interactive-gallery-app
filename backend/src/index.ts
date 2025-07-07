bc8d47ed3e6024a2fd387bd55d701de7d6adf6fd
chally-lang <54439978+chally-lang@users.noreply.github.com>
2025-07-07 16:41:21 +0100

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,7 +3,8 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = parseInt(process.env.PORT as string, 10) || 5000;
+
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
